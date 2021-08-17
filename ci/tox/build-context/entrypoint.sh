00e99af77cf719a1707b1fd8aac47fbab39370ea
Bartosz Siwinski <bartsiwinski@gmail.com>
2021-08-17 15:31:36 +0200

    Updated bash script.

--- ci/tox/build-context/entrypoint.sh
+++ ci/tox/build-context/entrypoint.sh
@@ -8,7 +8,12 @@
 set -e -u -x
 
 $PYTHON -m pip install --user tox
-TOXENV="pep8,py${PYTHON_VERSION/./}" tox
+
+if "${CHECK_COVERAGE:-false}" = true; then
+  TOXENV="pep8,py${PYTHON_VERSION/./},coverage" tox
+else
+  TOXENV="pep8,py${PYTHON_VERSION/./}" tox
+fi
 
 $PYTHON setup.py install --user
 git machete --version
